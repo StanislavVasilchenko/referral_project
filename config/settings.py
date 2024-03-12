3cb8193891861257148a8c6a1e3ce6a86388fc17
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-12 21:32:59 +0300

    create private key file

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from private_key import DB_NAME, DB_USER
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,8 +81,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'referal_proj',
-        'USER': 'postgres'
+        'NAME': DB_NAME,
+        'USER': DB_USER
     }
 }
 
