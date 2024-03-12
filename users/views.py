f3e8e9e1ed239bbec35cd34adca707faf7f11ff2
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-12 22:32:17 +0300

    create register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserRegistrationsAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            user = User.objects.create(
+                email=request.data['email']
+            )
+
+            user.set_password(request.data['password'])
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
