8ccc2ffb2588d449ac1d7607fa8a60883487a8a0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-14 12:49:41 +0300

    add ReferralCodeDelete

--- referral_code/urls.py
+++ referral_code/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from referral_code.apps import ReferralCodeConfig
-from referral_code.views import ReferralCodeCreateAPIView
+from referral_code.views import ReferralCodeCreateAPIView, ReferralCodeDelete
 
 app_name = ReferralCodeConfig.name
 
 urlpatterns = [
-    path('create/', ReferralCodeCreateAPIView.as_view(), name='referral-code-create')
+    path('create/', ReferralCodeCreateAPIView.as_view(), name='referral-code-create'),
+    path('delete/<int:pk>/', ReferralCodeDelete.as_view(), name='referral-code-delete')
 ]
