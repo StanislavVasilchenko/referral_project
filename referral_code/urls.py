7992b31df4c6d10694fca8279ae9fe902543a9d7
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-14 17:41:43 +0300

    add getcode

--- referral_code/urls.py
+++ referral_code/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from referral_code.apps import ReferralCodeConfig
-from referral_code.views import ReferralCodeCreateAPIView, ReferralCodeDelete
+from referral_code.views import ReferralCodeCreateAPIView, ReferralCodeDelete, ReferralCodeGetAPIView
 
 app_name = ReferralCodeConfig.name
 
 urlpatterns = [
     path('create/', ReferralCodeCreateAPIView.as_view(), name='referral-code-create'),
-    path('delete/<int:pk>/', ReferralCodeDelete.as_view(), name='referral-code-delete')
+    path('delete/<int:pk>/', ReferralCodeDelete.as_view(), name='referral-code-delete'),
+    path('getcode/', ReferralCodeGetAPIView.as_view(), name='referral-code-get')
 ]
