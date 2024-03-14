77745638cf9f4fc3263e3cdab01eed6f1a189a85
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-14 12:33:17 +0300

    create ReferralCodeCreateAPIView

--- referral_code/views.py
+++ referral_code/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from referral_code.models import ReferralCode
+from referral_code.serializers import ReferralSerializer
+
+
+class ReferralCodeCreateAPIView(generics.CreateAPIView):
+    queryset = ReferralCode.objects.all()
+    serializer_class = ReferralSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        if not self.queryset.filter(user_owner=request.user, is_active=True).exists():
+            ref_code = ReferralCode.objects.create(
+                user_owner=self.request.user,
+                name=request.data.get('name'),
+                lifetime=request.data.get('lifetime')
+            )
+            ref_code.save()
+            return Response(self.serializer_class(ref_code).data, status=status.HTTP_201_CREATED)
+        return Response({'message': 'You have active code'}, status=status.HTTP_400_BAD_REQUEST)
