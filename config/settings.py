0b24bf7e3bfd17c552d2328818d768a3097320a0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-17 01:01:48 +0300

    add db settings

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': DB_NAME,
-        'USER': DB_USER
+        'USER': DB_USER,
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db',
+
     }
 }
 
