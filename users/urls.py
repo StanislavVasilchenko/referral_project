2f1a7f1af95213194df1a008cc85f9378ac752e2
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-16 18:16:53 +0300

    add UserDetailAPIView

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserRegistrationsAPIView
+from users.views import UserRegistrationsAPIView, UserDetailAPIView
 
 app_name = UsersConfig.name
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('registration/', UserRegistrationsAPIView.as_view(), name='index')
+    path('registration/', UserRegistrationsAPIView.as_view(), name='registration_user'),
+    path('referral/<int:pk>/', UserDetailAPIView.as_view(), name='sub_list'),
 ]
