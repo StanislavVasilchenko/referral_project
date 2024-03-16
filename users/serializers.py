03532540c4b5a5eab223a5db5ac17903b8d6d0b8
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-16 18:16:23 +0300

    create UserDetailSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from users.models import User
 
@@ -6,4 +7,15 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ('password',)
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    subscribers = SerializerMethodField()
+
+    def get_subscribers(self, obj):
+        return UserSerializer(User.objects.filter(subscriber_id=obj.id), many=True).data
+
+    class Meta:
+        model = User
+        fields = ('id', 'subscribers')
