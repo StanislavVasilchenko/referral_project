560b7ac10b41775276e735ac0ef0e3ba0d5699ab
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-16 11:48:38 +0300

    add subscribe url

--- referral_code/urls.py
+++ referral_code/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from referral_code.apps import ReferralCodeConfig
-from referral_code.views import ReferralCodeCreateAPIView, ReferralCodeDelete, ReferralCodeGetAPIView
+from referral_code.views import ReferralCodeCreateAPIView, ReferralCodeDelete, ReferralCodeGetAPIView, subscribe
 
 app_name = ReferralCodeConfig.name
 
 urlpatterns = [
     path('create/', ReferralCodeCreateAPIView.as_view(), name='referral-code-create'),
     path('delete/<int:pk>/', ReferralCodeDelete.as_view(), name='referral-code-delete'),
-    path('getcode/', ReferralCodeGetAPIView.as_view(), name='referral-code-get')
+    path('getcode/', ReferralCodeGetAPIView.as_view(), name='referral-code-get'),
+    path('sub/', subscribe, name='referral-sub')
 ]
