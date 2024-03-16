8d05c6a0f4d201f1fcc79ec26ee473f973e1ae8f
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-16 11:46:16 +0300

    add subscriber field and remove is_referral field

--- users/models.py
+++ users/models.py
@@ -12,13 +12,13 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True, verbose_name='email address')
     is_referrer = models.BooleanField(default=False, verbose_name='referrer', **NULLABLE)
-    is_referral = models.BooleanField(default=False, verbose_name='referral', **NULLABLE)
+    subscriber = models.ForeignKey('self', **NULLABLE, on_delete=models.SET_NULL)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     def __str__(self):
-        return f'{self.email} - (referrer: {self.is_referrer}, referral: {self.is_referral})'
+        return f'{self.email} - (referrer: {self.is_referrer}, referral: {self.subscriber})'
 
     class Meta:
         verbose_name = 'user'
