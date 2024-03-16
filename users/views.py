be9c99b6f20ff9788a58f79056866e38cd0ffdb7
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-16 18:16:39 +0300

    create UserDetailAPIView

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserDetailSerializer
 
 
 class UserRegistrationsAPIView(generics.CreateAPIView):
@@ -21,3 +22,9 @@ class UserRegistrationsAPIView(generics.CreateAPIView):
             user.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAuthenticated]
